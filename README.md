de7d0225d41ab9317606a727a3f16bac37b53171
MiguelBarreraD <diazbarrera90355@gmail.com>
2024-03-14 14:32:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# APLICACI-N_SEGURA-
---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import static spark.Spark.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        port(getPort());
+
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+        secure("certificados/ecikeystore.p12", "123456", null, null);
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
