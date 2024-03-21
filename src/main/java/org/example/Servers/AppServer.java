7301e49057f138e59441483d9c1d43dd3225734b
MiguelBarreraD <diazbarrera90355@gmail.com>
2024-03-21 10:46:13 -0500

    Cambio de contraseña

--- src/main/java/org/example/Servers/AppServer.java
+++ src/main/java/org/example/Servers/AppServer.java
@@ -1,38 +1,38 @@
-package org.example.Servers;
+    package org.example.Servers;
 
-import org.example.Authentication.AuthenticationService;
+    import org.example.Authentication.AuthenticationService;
 
-import static spark.Spark.*;
-public class AppServer {
+    import static spark.Spark.*;
+    public class AppServer {
 
-    private static AuthenticationService authenticationService = new AuthenticationService();
-    public static void main(String[] args) {
-        staticFiles.location("/public");
-        port(getPort());
+        private static AuthenticationService authenticationService = new AuthenticationService();
+        public static void main(String[] args) {
+            staticFiles.location("/public");
+            port(getPort());
 
-        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
-        secure("certificados/ecikeystore.p12", "123456", null, null);
+            //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+            secure("certificados/ecikeystore.p12", "123456", null, null);
 
-        get("/login", (req, res) -> {
-            String user = req.queryParams("username");
-            String password = req.queryParams("password");
-            if (authenticationService.authenticate(user, password)) {
-                byte[] qrCodeImage = SecureURLReader.readExternalURL();
-                res.type("image/png");
-                res.status(200); 
-                return qrCodeImage;
-            } else {
-                res.status(401); 
-                return "Authentication failed";
-            }
-        });
-    }
-
-
-    static int getPort() {
-        if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
+            get("/login", (req, res) -> {
+                String user = req.queryParams("username");
+                String password = req.queryParams("password");
+                if (authenticationService.authenticate(user, password)) {
+                    byte[] qrCodeImage = SecureURLReader.readExternalURL();
+                    res.type("image/png");
+                    res.status(200); 
+                    return qrCodeImage;
+                } else {
+                    res.status(401); 
+                    return "Authentication failed";
+                }
+            });
         }
-        return 5000; 
+
+
+        static int getPort() {
+            if (System.getenv("PORT") != null) {
+                return Integer.parseInt(System.getenv("PORT"));
+            }
+            return 5000; 
+        }
     }
-}
